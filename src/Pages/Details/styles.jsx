a0efc76f0fc60e5bdefce82d744c8eb38a1e457b
samrjj <45480413+samrjj32@users.noreply.github.com>
2021-09-06 12:06:31 +0530

    Update styles.jsx

--- src/Pages/Details/styles.jsx
+++ src/Pages/Details/styles.jsx
@@ -1,6 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
+  main:{
+    minHeight:"100vh"
+  },
   mainContainer: {
     display: "flex",
     flexWrap: "wrap",
